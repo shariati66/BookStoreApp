71d0d02deb33b946b1f43cc85a64fe936fa3c10f
shariati66 <shariati1987@gmail.com>
2024-08-01 15:42:53 +0330

    add data annotation for test

---
+++ BookStoreApp/Models/DataAnnotationTestViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookStoreApp.Models
+{
+    public class DataAnnotationTestViewModel
+    {
+        [Required(ErrorMessage ="this is required.")]
+        public string Name { get; set; }
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [RegularExpression(@"^09\d{9}")]
+        [Phone]
+        [Display(Name ="Phone Number")]
+        public string PhoneNumber { get; set; }
+        [EmailAddress]
+        public string Email { get; set; }
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+    }
+}
