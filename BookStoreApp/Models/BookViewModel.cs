58e312b0d86efdcf0e8b5ba08947c1ad18132d69
shariati66 <shariati1987@gmail.com>
2024-08-01 10:06:28 +0330

    Add project files.

---
+++ BookStoreApp/Models/BookViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStoreApp.Models
+{
+    public class BookViewModel
+    {
+        [Range(1,int.MaxValue,ErrorMessage ="Id must greater than One")]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="name is required")]
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public decimal Price { get; set; }
+        [Required(ErrorMessage ="Publish date must be added")]
+        public DateTime PublishDate { get; set; }
+    }
+}
